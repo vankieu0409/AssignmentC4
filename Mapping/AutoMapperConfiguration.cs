9fd6520a5f4c2aac466a8e84209e7f18e7a23f74
Nguyen Kieu <vankieu0409@gmail.com>
2022-04-19 00:29:40 +0700

    maping PRoductcart vs cart

--- Mapping/AutoMapperConfiguration.cs
+++ Mapping/AutoMapperConfiguration.cs
@@ -14,6 +14,7 @@ public class AutoMapperConfiguration : Profile
         CreateMap<Products, ProductViewModelsCart>().ReverseMap();
         CreateMap<Customer, CustomerViewModel>().ReverseMap();
         CreateMap<ProductCUDViewModel, CustomerViewModel>().ReverseMap();
-        
+        CreateMap<Carts, ProductViewModelsCart>().ReverseMap();
+
     }
 }
